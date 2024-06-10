41afd31ed82ef77092f2f9c7ab37a032a37a3655
Bhavya <kachhadiyabhavya>
2024-06-10 21:44:06 +0530

    add: code

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const os = require('os')
+const app = express()
+const port = 3000
+
+app.get('/user', (req, res) => {
+   const userInfo=  os.userInfo()
+  res.send(userInfo.username)
+})
+app.get('/chip', (req, res) => {
+   const userInfo=  os.cpus()
+  res.send(userInfo[0].model)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
